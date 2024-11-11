40c2178f2a6e7c0533a93ce9b18e6da18f796353
ayoubbelhaj <ayoubbelhaj2017@gmail.com>
2024-11-11 14:34:31 +0100

    refactor(header): update header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -17,27 +17,28 @@ function SignOut() {
 }
 
 const Header = async (props: Props) => {
-    
+
     const session = await auth();
 
     return (
-        
-            <header className='border bottom-1'>
-                <nav className='bg-white border-gray-200 px-4 py-2.5'>
-                    <div className='flex flex-wrap justify-between items-center max-w-screen-xl'>
-                        <h1>AI Form Builder</h1>
-                    
+
+        <header className='border bottom-1'>
+            <nav className='bg-white border-gray-200 px-4 py-2.5'>
+                <div className='flex flex-wrap justify-between items-center max-w-screen-xl'>
+                    <h1>AI Form Builder</h1>
+
                     {
                         session?.user ? (
-                            <div className='flex items-center gap-4'><SignOut/>{session.user.name && session.user.image && <Image className='rounded-full' src={session.user.image} alt={session.user.name} width={32} height={32}/>}</div>
+                            <div className='flex items-center gap-4'><Link href="/view-forms">
+                                <Button variant="outline">Dashboard</Button></Link><SignOut />{session.user.name && session.user.image && <Image className='rounded-full' src={session.user.image} alt={session.user.name} width={32} height={32} />}</div>
                         ) : (
                             <Link href={"/api/auth/signin"}><Button>Sign in</Button></Link>
                         )
                     }
-                    </div>
-                </nav>
-            </header>
-        
+                </div>
+            </nav>
+        </header>
+
     )
 }
 
